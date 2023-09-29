ae2d3e98b5e0e0930cad6e6e20d921aadf9af41c
Mayur Hanwate <70631103+lordbakyarou@users.noreply.github.com>
2023-09-29 16:39:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+const h1 = document.getElementById("values");
+h1.innerText = `Width: ${window.screen.width} and Height: ${window.screen.height}`;
+
+window.addEventListener("resize", () => {
+ h1.innerText = `Width: ${window.screen.width} and Height: ${window.screen.height}`;
+});
